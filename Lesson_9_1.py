2fbb8c6cbb8fe02ade258e92f2e0303d60a3f646
K <11325990+KatanCamui@users.noreply.github.com>
2023-06-03 11:59:18 +0800

    modify

--- Lesson_9_1.py
+++ Lesson_9_1.py
@@ -2,3 +2,19 @@ import streamlit as st
 
 st.write("### Hello World Robert")
 st.write("Hello **Robert**")
+
+"### Hello World Robert"
+"Hello **Robert**"
+
+st.markdown("Hello **World**!")
+st.divider()
+st.title("這是app的title")
+st.divider()
+st.header("這是header")
+st.divider()
+st.subheader("這是subheader")
+st.divider()
+st.caption("這是caption")
+st.divider()
+st.code("a=123")
+st.divider()
