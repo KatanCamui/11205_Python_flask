0387ea023a4a51c5a7dc95cc678fabb36bb8c78b
K <11325990+KatanCamui@users.noreply.github.com>
2023-06-03 16:20:10 +0800

    modify

--- Lesson_9_11.py
+++ Lesson_9_11.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import streamlit as st
+import numpy
 
 url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
 response = requests.request('GET',url)
@@ -18,6 +19,9 @@ dataFrame = pd.DataFrame(data=all_data,columns=['sna','tot','sbi','sarea','mday'
 dataFrame.columns = ["站點名稱","車數","可借","行政區","時間","地址","可還","狀態"]
 dataFrame1 = dataFrame.set_index("站點名稱")
 
+group_data = dataFrame.groupby('行政區').sum()
+areas = group_data.index.to_numpy().tolist()
+
 min,max = st.slider(
     '請選擇可借的(<=數量)',
     0, 100, (0, 100))
@@ -25,6 +29,10 @@ min,max = st.slider(
 mask = (min <=dataFrame1['可借'])  & (dataFrame1['可借'] <= max) 
 mask_dataFrame = dataFrame1[mask]
 count = mask_dataFrame["車數"].count()
+
+option = st.selectbox('行政區域:', areas)
+st.write('你選擇:', option)
+
 st.write("符合條件的站點數:" , count)
 st.dataframe(mask_dataFrame)
 
